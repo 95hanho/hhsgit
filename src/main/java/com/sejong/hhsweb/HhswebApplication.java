88f81948eb821913e264a0eadf3da6d2399fa030
95hanho <hosung6674@naver.com>
2020-10-23 14:55:06 +0900

    git start

---
+++ src/main/java/com/sejong/hhsweb/HhswebApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.hhsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sejong/hhsweb/HhswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejong.hhsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
